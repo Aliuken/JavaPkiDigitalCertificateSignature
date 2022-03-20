724be6f85b8d249d5041a49b360ce07715d9b899
Aliuken <alber84ou@gmail.com>
2022-03-20 20:32:17 +0100

    Add files via upload

--- CertificateData.java
+++ CertificateData.java
@@ -4,7 +4,6 @@ import java.security.PrivateKey;
 import java.security.cert.Certificate;
 import java.security.cert.X509Certificate;
 
-public record CertificateData(PrivateKey privateKey, Certificate[] certificateChain, X509Certificate publicCertificate,
-                              String subjectAlias) {
+public record CertificateData(PrivateKey privateKey, Certificate[] certificateChain, X509Certificate publicCertificate, String subjectAlias) {
 
 }
